8de473bc93f3c69ef95fb66a8b98b129e4368efd
phengtola <tolapheng99@gmail.com>
2017-07-15 14:49:34 +0700

    first commit

---
+++ src/main/java/com/phengtola/spring/SpringBootMultipleMavenModulesApplication.java
@@ -0,0 +1,12 @@
+package com.phengtola.spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootMultipleMavenModulesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootMultipleMavenModulesApplication.class, args);
+	}
+}
